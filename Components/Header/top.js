46563e7bd0b35b5b214b80c83b93fdb0c4b134ff
unknown <hoseinia0870@gmail.com>
2024-03-10 00:11:18 +0330

    last computer update

--- Components/Header/top.js
+++ Components/Header/top.js
@@ -8,9 +8,12 @@ import Link from "next/link";
 import { useAuth } from "@/context/authContext";
 import UserDropDown from "./userDropDown";
 import { getUserQuickInfo } from "@/api/user/user";
+import { url } from "@/api/domain";
+import { getCookie } from "@/api/auth";
 
 const Top = ({categories}) => {
 
+  const token = getCookie('token')
   const {isLoggedIn} = useAuth();
   const [openCategory, setOpenCategory] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -98,6 +101,7 @@ const Top = ({categories}) => {
           <li
            className={styles.top__list__category}
            onClick={handleOpenCategory}
+           style={isMobile ? {display : "none"} : {}}
           >
             <span>دسته بندی ها</span>
           </li>
@@ -138,8 +142,8 @@ const Top = ({categories}) => {
             <Link href="/about">درباره ما</Link>
           </li>
         </ul>
-        <div className={styles.top__sign}>
-          <div className={styles.signbutton}>
+        <div className={styles.top__sign} style={isMobile ? {display: "none"} : {}}>
+          <div className={styles.signbutton} style={isMobile ? {display: "none"} : {}}>
             {isLoggedIn == false ?<Link className={styles.signbuttonbutton} href="/login">ورود / عضویت</Link>:
               <div className={styles.usersignwrapper}>
                 <button onClick={handleShowUserDetail}>
@@ -157,7 +161,7 @@ const Top = ({categories}) => {
               </div>
             }
           </div>
-          <div className={styles.basket}>
+          <div className={styles.basket} style={isMobile ? {display: "none"} : {}}>
             <Link href={"/cart"}>
               <Image src={basket} alt="" />
             </Link>
@@ -167,6 +171,37 @@ const Top = ({categories}) => {
           </div>
         </div>
       </div>
+      {isMobile == true ? 
+          <div className={styles.topMobile + " bg-white p-3 w-full flex z-50 items-center justify-between"}>
+            {isLoggedIn == false ? 
+              <Link href={"/login"}>
+                <div className={styles.mitem}>
+                  <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>        
+                </div>
+              </Link>
+              : isLoggedIn == true ? 
+              <a target="__blank" href={url + 'login-to-panel?token=' + token} className={styles.link}>
+                <div className={styles.mitem}>
+                  <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>        
+                </div>
+              </a>
+              : ""
+            }
+            <Link href={"/cart"}>
+              <div className={styles.mitem}>
+                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>        
+              </div>
+            </Link>
+            <div className={styles.mitem}>
+              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"/></svg>        
+            </div>
+            <Link href={"/"}>
+              <div className={styles.mitem}>
+                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
+              </div>
+            </Link>
+          </div> : ""
+      }
     </>
   );
 };
